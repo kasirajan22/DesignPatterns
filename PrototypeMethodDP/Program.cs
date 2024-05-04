1f3e8f815700582dc52b8031e5bc9c00e7558cb4
magizhan <kasirajan22@hotmail.com>
2024-05-05 05:24:48 +0530

    Singleton & Prototype

---
+++ PrototypeMethodDP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// Interface for the prototype
+public interface IGraphicElement
+{
+	IGraphicElement Clone();
+	void Display();
+	// Other methods for customization can be added here
+}
+
+// Concrete prototype representing a graphic element
+public class Logo : IGraphicElement
+{
+	private string name;
+	private string color;
+
+	public Logo(string name, string color)
+	{
+		this.name = name;
+		this.color = color;
+	}
+
+	public IGraphicElement Clone()
+	{
+		// Create a shallow copy of the object
+		return (IGraphicElement)MemberwiseClone();
+	}
+
+	public void Display()
+	{
+		Console.WriteLine($"Logo: {name}, Color: {color}");
+	}
+
+	// Additional methods for customization can be added here
+}
+
+// Client code demonstrating the use of the Prototype pattern
+class Program
+{
+	static void Main()
+	{
+		// Create an initial logo prototype
+		IGraphicElement originalLogo = new Logo("OriginalLogo", "Blue");
+		
+		// Clone the original logo to create a new customized logo
+		IGraphicElement customizedLogo = originalLogo.Clone();
+		
+		// Display the original and customized logos
+		Console.WriteLine("Original Logo:");
+		originalLogo.Display();
+
+		Console.WriteLine("\nCustomized Logo:");
+		customizedLogo.Display();
+	}
+}
---
+++ SingletonMethosDP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Singleton
+{
+	// static class
+	private static Singleton? instance;
+
+	private Singleton()
+	{
+		Console.WriteLine("Singleton is Instantiated.");
+	}
+
+	public static Singleton GetInstance()
+	{
+		if (instance == null)
+			instance = new Singleton();
+		return instance;
+	}
+
+	public void DoSomething()
+	{
+		Console.WriteLine("Something is Done.");
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Singleton.GetInstance().DoSomething();
+	}
+}
